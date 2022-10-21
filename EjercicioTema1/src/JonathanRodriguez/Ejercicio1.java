a7856f8aeba3169eed4c7e9c3afce19e14491de6
Jonathan Rodriguez <rodriguezjmm@hotmail.com>
2022-10-21 15:56:19 -0400

    Ejercicio Tema 1 Terminado

---
+++ EjercicioTema1/src/JonathanRodriguez/Ejercicio1.java
@@ -0,0 +1,34 @@
+package JonathanRodriguez;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        //números
+        byte num1 = 9;
+        short num2 = 39;
+        int num3 = 1983;
+        long num4 = 9223372036854775807L;
+        float num5 = 1.78f;
+        double num6 = 1.22014E-308;
+        
+        //caracter
+        char letra = 'J';
+        
+        //cadenas
+        String nombre = "Jonathan";
+        
+        //booleanos
+        boolean isDeveloper = true;
+        boolean isLazzy = false;
+    
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+        System.out.println(num6);
+        System.out.println(letra);
+        System.out.println(isDeveloper);
+        System.out.println(isLazzy);
+        System.out.println(nombre);
+    }
+}
