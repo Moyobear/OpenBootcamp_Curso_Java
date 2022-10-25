5d6e31d55ed3d2e4e58dcf29ec90938d5f02903e
Jonathan Rodriguez <rodriguezjmm@hotmail.com>
2022-10-25 11:15:43 -0400

    Delete Ejercicio5 directory

--- Ejercicio5/src/Coche.java
+++
@@ -1,9 +0,0 @@
-public class Coche {
-    String fabricante;
-    String modelo;
-    int year;
-    
-    public Coche(){};
-    public Coche(String fabricante, String modelo, int year) {};
-    
-}
--- Ejercicio5/src/CocheCRUD.java
+++
@@ -1,7 +0,0 @@
-public interface CocheCRUD {
-    void save(Coche coche);
-    
-    void delete(Coche coche);
-    
-    void findAll();
-}
